d4fd0fecffe69675ef29aa92ef232e632361e817
andrea costantinis <andrea.costantinis@atlantide-web.it>
2020-03-24 09:47:58 +0100

    test 6

--- atlantide_connector/events.py
+++ atlantide_connector/events.py
@@ -1,8 +1,28 @@
-from odoo.addons.connector.event import on_record_create
+from odoo.addons.component.core import Component
+from odoo.addons.component_event import skip_if
 
-@on_record_create()
+class AtlantideListener(Component):
+    _name = 'atlantide.event.listener'
+    _inherit = 'base.connector.listener'
 
-def procesa_creacion_de_usuarios(session, model_name, record_id, vals):
+    @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+    def on_record_create(self, record, fields=None):
+        """ Called when a record is created """
         f = open("mylog.txt", "a")
         f.write("Create!")
-        f.close()
+        f.close()		
+
+    @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+    def on_record_write(self, record, fields=None):
+        f = open("mylog.txt", "a")
+        f.write("Write!")
+        f.close()		
+
+    def on_record_unlink(self, record):
+        with record.backend_id.work_on(record._name) as work:
+            external_id = work.component(usage='binder').to_external(record)
+            if external_id:
+                f = open("mylog.txt", "a")
+                f.write("Delete!")
+                f.close()		
+

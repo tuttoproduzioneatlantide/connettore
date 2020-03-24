55b222ec27e3ad41c44340ec90c12ccc35d58e95
andrea costantinis <andrea.costantinis@atlantide-web.it>
2020-03-24 11:11:04 +0100

    show record

--- atlantide_connector/events.py
+++ atlantide_connector/events.py
@@ -4,18 +4,19 @@ from odoo.addons.component_event import skip_if
 class AtlantideListener(Component):
     _name = 'atlantide.event.listener'
     _inherit = 'base.connector.listener'
+	_apply_on = ['res.users']
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_create(self, record, fields=None):
         """ Called when a record is created """
         f = open("mylog.txt", "a")
-        f.write("Create!")
+        f.write("Create! {0} \n".format(record))
         f.close()		
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_write(self, record, fields=None):
         f = open("mylog.txt", "a")
-        f.write("Write!")
+        f.write("Write! {0} \n".format(record))
         f.close()		
 
     def on_record_unlink(self, record):
@@ -23,6 +24,6 @@ class AtlantideListener(Component):
             external_id = work.component(usage='binder').to_external(record)
             if external_id:
                 f = open("mylog.txt", "a")
-                f.write("Delete!")
+                f.write("Write! {0} \n".format(record))
                 f.close()		
 

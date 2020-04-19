bd784a8b3a343c7cfe7bfad946e33fc1b584bb7e
andrea costantinis <andrea.costantinis@atlantide-web.it>
2020-04-19 11:36:06 +0200

    .

--- atlantide_connector/logmodel.py
+++ atlantide_connector/logmodel.py
@@ -1,14 +1,21 @@
 from odoo import models,fields
 
 class LogForAtlantide(models.Model):
-    _name = 'atlantide.changes_for_atlantide'
+    _name = 'x_log_changes_for_atlantide'
     
-    model_name = fields.Char(string="model name", index=True, size=255)
-    model_id    = fields.Integer(string="model idd", index=True)
-    action_type    fields.Char(string="action type", index=True, size=1)
-    action_timestamp    = fields.Datetime(string="action time")
-    action_performed_by_user = fields.Integer(string="action was perform by user with this id")
-    is_processed  = fields.Boolean(string="already processed by atlantide")
-    atlantide_process_failed  = fields.Boolean(string="processed by atlantide with errors")
-    process_error_description = fields.Text(string="description of atlantide process error")
+    x_model_name = fields.Char(string="model name", index=True, size=255)
+	
+    x_model_id    = fields.Integer(string="model idd", index=True)
+    
+	x_action_type  =  fields.Char(string="action type", index=True, size=1)
+    
+	x_action_timestamp    = fields.Datetime(string="action time")
+    
+	x_action_performed_by_user = fields.Integer(string="action was perform by user with this id")
+    
+	x_is_processed  = fields.Boolean(string="already processed by atlantide")
+    
+	x_atlantide_process_failed  = fields.Boolean(string="processed by atlantide with errors")
+    
+	x_process_error_description = fields.Text(string="description of atlantide process error")
     

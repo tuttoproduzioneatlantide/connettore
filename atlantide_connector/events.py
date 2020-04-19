9829a9bc8e082449fed94dc4396d362232d354dc
andrea costantinis <andrea.costantinis@atlantide-web.it>
2020-04-19 12:49:47 +0200

    .

--- atlantide_connector/events.py
+++ atlantide_connector/events.py
@@ -1,5 +1,6 @@
 from odoo.addons.component.core import Component
 from odoo.addons.component_event import skip_if
+#import datetime
 
 class AtlantideListener(Component):
     _name = 'atlantide.event.listener'
@@ -11,16 +12,25 @@ class AtlantideListener(Component):
         """ Called when a record is created """
         f = open("mylog.txt", "a")
         f.write("Create! {0} Record: {1} Self: {2} Fields: {3}\n".format(record, type(record), type(self), type(fields)))
-        f.close()		
+        f.close()
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_write(self, record, fields=None):
         f = open("mylog.txt", "a")
-        f.write("Write! {0}  Record: {1} Self: {2} Fields: {3}\n".format(record, type(record), type(self), type(fields)))
-        f.close()		
-
+        f.write("Write! {0}  Record: {1} Self: {2} Fields: {3} Nostro: {4}\n".format(record, type(record), type(self), type(fields), type(self.env['x_log_changes_for_atlantide'])))
+        f.close()
+        #self.env['x_log_changes_for_atlantide'].create({})
+        #    x_model_name: 'res.users.2',
+        #    x_model_id: self.id,
+        #    x_action_type: 'u',
+        #    #x_action_timestamp: datetime.datetime.now()
+        #    x_action_performed_by_user: 'andrea',
+        #    x_is_processed: False,
+        #    x_atlantide_process_failed: False,
+        #    x_process_error_description: ''
+        #})
     def on_record_unlink(self, record):
         f = open("mylog.txt", "a")
         f.write("Write! {0}  Record: {1} Self: {2} \n".format(record, type(record), type(self)))
-        f.close()		
+        f.close()
 

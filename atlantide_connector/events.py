d44d459198f0c0453bbbdb921be1a671086beec7
andrea costantinis <andrea.costantinis@atlantide-web.it>
2020-04-19 10:19:55 +0200

    .

--- atlantide_connector/events.py
+++ atlantide_connector/events.py
@@ -10,17 +10,17 @@ class AtlantideListener(Component):
     def on_record_create(self, record, fields=None):
         """ Called when a record is created """
         f = open("mylog.txt", "a")
-        f.write("Create! {0} \n".format(record))
+        f.write("Create! {0} Record: {1} Self: {2} Fields: {3}\n".format(record, type(record), type(self), type(fields)))
         f.close()		
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_write(self, record, fields=None):
         f = open("mylog.txt", "a")
-        f.write("Write! {0} \n".format(record))
+        f.write("Write! {0}  Record: {1} Self: {2} Fields: {3}\n".format(record, type(record), type(self), type(fields)))
         f.close()		
 
     def on_record_unlink(self, record):
         f = open("mylog.txt", "a")
-        f.write("Write! {0} \n".format(record))
+        f.write("Write! {0}  Record: {1} Self: {2} \n".format(record, type(record), type(self)))
         f.close()		
 

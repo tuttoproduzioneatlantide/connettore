19b789421b6367b65bb70b7249f1a2d773525cec
andrea costantinis <andrea.costantinis@atlantide-web.it>
2020-04-19 16:46:27 +0200

    .

--- atlantide_connector/events.py
+++ atlantide_connector/events.py
@@ -9,28 +9,38 @@ class AtlantideListener(Component):
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_create(self, record, fields=None):
-        """ Called when a record is created """
-        f = open("mylog.txt", "a")
-        f.write("Create! {0} Record: {1} Self: {2} Fields: {3}\n".format(record, type(record), type(self), type(fields)))
-        f.close()
+        self.env['x_log_changes_for_atlantide'].create({
+            'x_model_name': record._name,
+            'x_model_id': record.id,
+            'x_action_type': 'a',
+            'x_action_timestamp': datetime.now(),
+            'x_action_performed_by_user': record.write_uid.ids[0],
+            'x_is_processed': False,
+            'x_atlantide_process_failed': False,
+            'x_process_error_description': ''
+        })
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_write(self, record, fields=None):
-        #f = open("mylog.txt", "a")
-        #f.write("Write! {0}  Record: {1} Self: {2} Fields: {3} Nostro: {4}\n".format(record, type(record), type(self), type(fields), type(self.env['x_log_changes_for_atlantide'])))
-        #f.close()
         self.env['x_log_changes_for_atlantide'].create({
-            'x_model_name': 'res.users.2',
-            'x_model_id': 32,
+            'x_model_name': record._name,
+            'x_model_id': record.id,
             'x_action_type': 'u',
             'x_action_timestamp': datetime.now(),
-            'x_action_performed_by_user': 1,
+            'x_action_performed_by_user': record.write_uid.ids[0],
             'x_is_processed': False,
             'x_atlantide_process_failed': False,
             'x_process_error_description': ''
         })
     def on_record_unlink(self, record):
-        f = open("mylog.txt", "a")
-        f.write("Write! {0}  Record: {1} Self: {2} \n".format(record, type(record), type(self)))
-        f.close()
+        self.env['x_log_changes_for_atlantide'].create({
+            'x_model_name': record._name,
+            'x_model_id': record.id,
+            'x_action_type': 'r',
+            'x_action_timestamp': datetime.now(),
+            'x_action_performed_by_user': record.write_uid.ids[0],
+            'x_is_processed': False,
+            'x_atlantide_process_failed': False,
+            'x_process_error_description': ''
+        })
 
